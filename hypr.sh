80e40a8d4c16c6d83b0740538922e646bb9db48b
NeondbPG <135182811+NeondbPG@users.noreply.github.com>
2024-06-27 07:47:10 +0000

    Update hypr.sh

--- hypr.sh
+++ hypr.sh
@@ -17,7 +17,7 @@ install_aur_helper() {
   makepkg -si
 }
 
-install_hypr() {
+install_hypr_cmake() {
   paru -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang hyprcursor hyprwayland-scanner hyprutils xcb-util-errors
 
   git clone --recursive https://github.com/hyprwm/Hyprland
@@ -28,4 +28,3 @@ install_hypr() {
 
 setup_ctl
 install_aur_helper
-install_hypr
